c072a64f8bed1a08e5f5c713b3ce88de62b97132
Sakaue Aoi <aoiue.w.bbb@gail.com>
2016-11-14 22:17:01 +0900

    no message

--- PasswordHacker/ViewController.swift
+++ PasswordHacker/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //結果表示ラベル
+    @IBOutlet var resultLabel1 : UILabel!
+    @IBOutlet var resultLabel2 : UILabel!
+    @IBOutlet var resultLabel3 : UILabel!
+    @IBOutlet var resultLabel4 : UILabel!
+    
+    //カウント用ラベル
+    @IBOutlet var countLabel : UILabel!
+    
+    //himitsupass
+    var password : Int = 3939
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,53 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func start(){
+        for i in 0...9999 {
+            
+            //解析中の数字を表示する
+            countLabel.text = String(i)
+            
+            //処理を0.0005秒、待機する
+            RunLoop.main.run(until: Date(timeIntervalSinceNow: 0.0005))
+            
+            //passwordとiが一致したら、、
+            if password == i {
+                
+                //passwordのけたごとの数字を入れる配列
+                var digits = [Int]()
+                
+                for _ in 0...3 {
+                    digits.append(password % 10) //j桁目の数字
+                    password = password / 10
+                    
+                }
+                
+                resultLabel1.text = String(digits[0])
+                resultLabel2.text = String(digits[1])
+                resultLabel3.text = String(digits[2])
+                resultLabel4.text = String(digits[3])
+                
+                NSLog("正解は%dです", i)
+            }
+            
+    }
+    }
+    
+    //初期化
+    @IBAction func reset() {
+        //passwordを3939に再設定する
+        password = 3939
+        
+        //xountlabelの表示を元に戻す
+        countLabel.text = "「START」ボタンを押して解析開始"
+        
+        //ラベルの表示を元に戻す
+        resultLabel1.text = "0"
+        resultLabel2.text = "0"
+        resultLabel3.text = "0"
+        resultLabel4.text = "0"
+    }
+    
 
 }
 
